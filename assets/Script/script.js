7d942b5e30107086da58b094c9900ba32a618df8
ZaraMr <zhra.mrdi2907@gmail.com>
2025-12-18 03:07:58 +0330

    Add Cart
    added cart and related js to open or close it (when out side of cart is clicked it will close) and related styles

--- assets/Script/script.js
+++ assets/Script/script.js
@@ -1,6 +1,10 @@
+"use strict";
+
 const menuBtn = document.querySelector(".menu");
 const menuList = document.querySelector(".nav-menu");
 const closeBtn = document.querySelector(".close-btn");
+const cartBtn = document.querySelector(".user-cart-btn");
+const cartBox = document.querySelector(".cart");
 
 const openMenu = function () {
   menuList.classList.add("show-mobile-menu");
@@ -17,3 +21,30 @@ menuBtn.addEventListener("click", openMenu);
 
 // Close menu when the close button is clicked
 closeBtn.addEventListener("click", closeMenu);
+
+// open cart when cart btn is clicked
+cartBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  cartBox.classList.toggle("cart-active");
+
+  // change cart btn color when cart is open
+  cartBtn.firstElementChild.classList.toggle("cart-btn-active");
+
+  // stop scrolling
+  document.body.classList.toggle("cart-active");
+});
+
+// remove cart when outside of cart and cartBtn clicked
+const handleOutsideClick = function (e) {
+  if (!cartBox.classList.contains("cart-active")) return;
+
+  if (e.target.closest(".cart")) {
+    return;
+  }
+
+  cartBox.classList.remove("cart-active");
+  cartBtn.firstElementChild.classList.remove("cart-btn-active");
+  document.body.classList.remove("cart-active");
+};
+
+document.addEventListener("click", handleOutsideClick);
